e813c6ef47770c49e1f84a585a05ccf480da9c85
Guilherme Romera <g.romera2018@icloud.com>
2026-09-23 17:51:10 -0300

    sintaxe com manipula;ção de strings

---
+++ exercicio-sintaxe.php
@@ -0,0 +1,11 @@
+<?php
+
+$meuNome = "Guilherme";
+$meuSobrenome = "Romera";
+$minhaIdade = 26;
+
+echo "Meu nome: " . $meuNome . "\n";
+echo "Meu sobrenome: " . $meuSobrenome . "\n";
+echo "Minha idade: " . $minhaIdade . "\n";
+
+echo "Meu nome completo é " .$meuNome . " " . $meuSobrenome . " e tenho " . $minhaIdade . " anos de idade.\n";

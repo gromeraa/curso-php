87c294c81c7e9a9b09be72f270a8644c07fc60c3
Guilherme Romera <g.romera2018@icloud.com>
2026-09-23 22:16:06 -0300

    echo array

---
+++ exercicio-sintaxe4.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$array = [1, 2, 2, 3, 4, 4, 5];
+$semDuplicatas = array_unique($array);
+
+echo "Array original: ";
+var_dump($array);
+
+echo "Array sem duplicatas: ";
+var_dump($semDuplicatas);

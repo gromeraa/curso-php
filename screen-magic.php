a21c4b899f405f4528504b62a61e2b932066a2c0
Guilherme Romera <g.romera2018@icloud.com>
2026-09-23 21:11:53 -0300

    Match expression

--- screen-magic.php
+++ screen-magic.php
@@ -3,6 +3,8 @@
 echo "Bem-vindo ao screen magic!
 ";
 
+$nomeFilme = "Se beber não case";
+$nomeFilme = "Star Wars";
 $nomeFilme = "Top Gun: Maverick";
 $anoLancamento = $argv[1] ?? 2022;
 $notaFilme = 8.8;
@@ -12,9 +14,17 @@ $incluidoNoPlano = $planoPrime || $anoLancamento < 2020;
 //echo "$anoLancamento";
 
 if ($anoLancamento > 2022) {
-    echo "O filme é um lançamento.";
+    echo "O filme é um lançamento.\n";
 } elseif ($anoLancamento > 2020 &&  $anoLancamento <= 2022) {
-    echo "O filme ainda é novo, mas não é um lançamento.";
+    echo "O filme ainda é novo, mas não é um lançamento.\n";
 } else {
-    echo "O filme é antigo.";
+    echo "O filme é antigo.\n";
 }
+
+$genero = match ($nomeFilme) {
+    "Top Gun: Maverick" => "Ação",
+    "Star Wars" => "Ficção Científica",
+    "Se beber não case" => "Comédia",
+};
+
+echo "O filme $nomeFilme é do gênero $genero.\n";

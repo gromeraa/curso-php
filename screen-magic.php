a1de836754052b0760fff6c56c34bb49743efdcc
Guilherme Romera <g.romera2018@icloud.com>
2026-09-23 21:02:19 -0300

    decisões

--- screen-magic.php
+++ screen-magic.php
@@ -9,4 +9,12 @@ $notaFilme = 8.8;
 $planoPrime = true;
 $incluidoNoPlano = $planoPrime || $anoLancamento < 2020;
 
-echo "$anoLancamento";
+//echo "$anoLancamento";
+
+if ($anoLancamento > 2022) {
+    echo "O filme é um lançamento.";
+} elseif ($anoLancamento > 2020 &&  $anoLancamento <= 2022) {
+    echo "O filme ainda é novo, mas não é um lançamento.";
+} else {
+    echo "O filme é antigo.";
+}
